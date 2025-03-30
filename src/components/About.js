805a985b6850f442bbb010199ecbffea518a3d8e
GGCav <jeffreyhe406@gmail.com>
2025-03-29 23:46:27 -0400

    profile image

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <section>
       <h2 className="section-title">ABOUT</h2>
-      <img src="/public/profile.jpg" alt="JinFeng He" className="profile-img" />
+      <img src={process.env.PUBLIC_URL + '/profile.jpg'} alt="JinFeng He" className="profile-img" />
       <p>A software developer based in Ithaca, USA specializing in full-stack development with expertise in distributed systems and cloud architecture.</p>
       <p>I believe that software development is both a science and an art. The way we express our creativity through code shall have no boundaries because when it's fueled by our true passions and beliefs it will lead to great results.</p>
       <p>In this day and age where technological innovation is driving us forward, one unique element to my development work is to incorporate scalable and efficient solutions to solve real-world problems. As a developer, I feel responsible and inspired for creating well-architected systems that provide value and positive impact through my work.</p>
